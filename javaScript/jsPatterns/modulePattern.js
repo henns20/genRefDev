c65a1d4a8024716c9f7b27c7e6f9fc5827a13326
John Hennelly <jjhennelly@gmail.com>
2014-11-11 03:04:24 -0500

    examples of module pattern

--- javaScript/jsPatterns/modulePattern.js
+++ javaScript/jsPatterns/modulePattern.js
@@ -71,4 +71,29 @@ var basketModule = (function(){
 
 })();
 
+// Using the module
 
+// basetModule returns an object with a pucblic API we can use 
+
+// purpose: 1. add 2 items 2. get # of items in basket 3. get total cost of items 4. show 2 methods or statements that will not work. 
+
+
+basketModule.addItem({
+	item: 'cucumber',
+	price: 0.5
+});
+
+basketModule.addItem({
+	item: 'apple',
+	price: 0.3
+});
+
+// get total cost and amount of items in the basket
+
+console.log(basketModule.getTotal());
+console.log(basketModule.getCount());
+
+// do a couple of things that won't work
+
+console.log(basketModule.basket);
+console.log(basket);
