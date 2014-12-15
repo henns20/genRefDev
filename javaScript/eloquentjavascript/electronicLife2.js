95bddbfdbea592fa067e42fea1d92cfd39ae8135
John Hennelly <jjhennelly@gmail.com>
2014-12-15 00:09:48 -0500

    updated files

---
+++ javaScript/eloquentjavascript/electronicLife2.js
@@ -0,0 +1,20 @@
+/**
+ * Created by john on 12/1/14.
+ */
+
+// Goal is to List How Book Has it from big Picture Point of View and the code. And any questions on the code.
+
+/* Outline
+Representing space
+* 1. Vector
+    : Constructor: this defines the cell* an ever changing cell so - it needs the approach of constructor*.? Represents the coordinate pairs.
+    faq - we use a simple type `
+*   : Vector prototype plus method. Modifies a current vector position
+* 2. Grid: object type that models the grid itself. Options: array of row arrays; or a single array
+*
+*   Constructor:
+*
+*
+*
+*
+* */
